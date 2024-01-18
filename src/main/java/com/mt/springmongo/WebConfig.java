fab629796e6f778eb18e1d89d393efe8fb7640f1
nfestaclass02 <137131319+nfestaclass02@users.noreply.github.com>
2024-01-18 16:11:39 -0500

    Update WebConfig.java

--- src/main/java/com/mt/springmongo/WebConfig.java
+++ src/main/java/com/mt/springmongo/WebConfig.java
@@ -1,4 +1,4 @@
-package com.mt.springmongo;
+package com.mt.springmongo;nnnn
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
